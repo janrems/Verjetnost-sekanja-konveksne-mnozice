91750e1368c570d76b9d2f5303fe092d2752370f
lencdo <dolenc94@gmail.com>
2018-01-02 12:14:53 +0100

    neki malga

--- Eksperimentalni_del/spremeni.m
+++ Eksperimentalni_del/spremeni.m
@@ -1,15 +1,9 @@
 function[a]=spremeni(p, theta)
 k=tan(theta);
-if theta<(pi/2)
+if theta<pi
     n=-p/sin((pi/2)-theta);
-elseif theta>=(pi/2) && theta<pi
-    n=p/sin(theta-pi/2);
-elseif theta>=pi && theta<(3*pi)/2
+elseif theta>=pi
     n=p/sin((3*pi)/2-theta);
-elseif theta>=(3*pi)/2
-    n=-p/sin(theta-(3*pi)/2);
 end
 a=[k, n];
 end
-  
-    
